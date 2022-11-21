2e12ced83869390689de8430d89dd0c0359dbd91
Eric R. Scott <scottericr@gmail.com>
2022-11-21 12:30:32 -0500

    remove island from analysis so I can add it as an example of branching

--- R/fit_models.R
+++ R/fit_models.R
@@ -1,10 +1,10 @@
 #log transformed height
 fit_lm_flipper <- function(data) {
-  lm(flipper_length_mm ~ body_mass_g + sex + species + island + species:sex, data = data)
+  lm(flipper_length_mm ~ body_mass_g + sex + species + species:sex, data = data)
   
 }
 
 #log-link glm (lognormal family distribution)
 fit_lm_bill <- function(data) {
-  lm(bill_length_mm ~ body_mass_g + sex + species + island + species:sex, data = data)
+  lm(bill_length_mm ~ body_mass_g + sex + species + species:sex, data = data)
 }
