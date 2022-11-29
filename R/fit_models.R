2835f304964bfb3dbec9a9700cb8796d8d209513
Eric R. Scott <scottericr@gmail.com>
2022-11-29 15:03:32 -0500

    make interaction explicit

--- R/fit_models.R
+++ R/fit_models.R
@@ -1,7 +1,6 @@
 #full model with body mass, sex, and their interaction
 fit_lm_full <- function(data) {
-  lm(flipper_length_mm ~ body_mass_g * sex, data = data)
-  
+  lm(flipper_length_mm ~ body_mass_g + sex + body_mass_g:sex, data = data)
 }
 
 #model without the interaction
