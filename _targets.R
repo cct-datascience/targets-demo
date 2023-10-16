8fe5563d34bbba80dda8f84b26212090bd47ea8a
Eric Scott <scottericr@gmail.com>
2023-10-16 15:08:34 -0400

    update to use crew

--- _targets.R
+++ _targets.R
@@ -1,28 +1,23 @@
 # Created by use_targets().
 # Follow the comments below to fill in this target script.
 # Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline 
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed. # nolint
+# library(tarchetypes) # Load other packages as needed. 
 
 # Set target options:
 tar_option_set(
   packages = c("tidyverse", "janitor", "bbmle"), # packages that your targets need to run
-  format = "rds" # default storage format
+  format = "rds", # default storage format
+  controller = crew::crew_controller_local(workers = 2), #run locally on 2 cores
   # Set other options as needed.
 )
 
-# tar_make_clustermq() configuration (okay to leave alone):
-options(clustermq.scheduler = "multicore")
-
-# tar_make_future() configuration (okay to leave alone):
-future::plan(future.callr::callr)
-
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed. # nolint
+# source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
--- run.R
+++ run.R
@@ -6,5 +6,3 @@
 # to learn about your options.
 
 targets::tar_make()
-# targets::tar_make_clustermq(workers = 2) # nolint
-# targets::tar_make_future(workers = 2) # nolint
