8d6d0c1a5b21f25013a53da23d852fb861cf5e5c
Eric Scott <scottericr@gmail.com>
2025-01-30 10:46:24 -0700

    targets no longer creates these files

--- run.R
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-
-# This is a helper script to run the pipeline.
-# Choose how to execute the pipeline below.
-# See https://books.ropensci.org/targets/hpc.html
-# to learn about your options.
-
-targets::tar_make()
