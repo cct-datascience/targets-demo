f323557d9604ad621334692b014fc46197170369
Eric Scott <scottericr@gmail.com>
2025-02-11 11:24:48 -0700

    rename function to follow  my own advice

--- _targets.R
+++ _targets.R
@@ -6,25 +6,22 @@
 # Load packages required to define the pipeline:
 library(targets)
 library(crew)
-# library(tarchetypes) # Load other packages as needed. 
 
 # Set target options:
 tar_option_set(
   packages = c("tidyverse", "janitor", "bbmle"), # packages that your targets need to run
-  format = "rds", # default storage format
-  controller = crew_controller_local(workers = 2), #run locally on 2 cores
+  controller = crew_controller_local(workers = 2), # run workflow in parallel
   # Set other options as needed.
 )
 
-# Run the R scripts in the R/ folder with your custom functions:
+# Source the R scripts in the R/ folder with your custom functions:
 tar_source()
-# source("other_functions.R") # Source other scripts as needed.
 
-# Replace the target list below with your own:
+# Workflow definition:
 list(
   tar_target(file, "data/penguins_raw.csv", format = "file"),
   tar_target(data, get_data(file)),
   tar_target(lm_full, fit_lm_full(data)),
-  tar_target(lm_noint, fit_lm_noint(data)),
-  tar_target(aic_tab, AICtab(lm_full, lm_noint))
+  tar_target(lm_main_effects, fit_lm_main_effects(data)),
+  tar_target(aic_tab, AICtab(lm_full, lm_main_effects))
 )
